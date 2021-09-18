7acb1e4b7caa25ee320174730e6c7a0769e09f87
Rincy Pradeep <rincypradeep15@gmail.com>
2021-09-18 19:55:56 +0530

    Change firebase config

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -3,14 +3,14 @@ import 'firebase/auth'
 import 'firebase/firebase'
 import 'firebase/storage'
 
-const firebaseConfig = {
-    apiKey: "AIzaSyDJwtAX4KowLiHJSa5PDAXMf0ZK-cr_Xbo",
-    authDomain: "netflix-clone-eca3d.firebaseapp.com",
-    projectId: "netflix-clone-eca3d",
-    storageBucket: "netflix-clone-eca3d.appspot.com",
-    messagingSenderId: "625107181776",
-    appId: "1:625107181776:web:7cb7aeff4f9c3c1729fed5",
-    measurementId: "G-WEVM52Q0QY"
+  const firebaseConfig = {
+    apiKey: "AIzaSyA8e_9bm3Th3X63Uoc5DDOVaoVIut6YjMA",
+    authDomain: "clone-net-f8687.firebaseapp.com",
+    projectId: "clone-net-f8687",
+    storageBucket: "clone-net-f8687.appspot.com",
+    messagingSenderId: "1016779839009",
+    appId: "1:1016779839009:web:1c3cc4068319684a103560",
+    measurementId: "G-TGP69NE8KF"
   };
 
   export default firebase.initializeApp(firebaseConfig)
